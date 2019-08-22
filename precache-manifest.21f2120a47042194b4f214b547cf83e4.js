c2d4167d5ea2c2f6ba050b0e0cd38ef93ebf2396
emilmannfeldt <emil.mannfeldt@gmail.com>
2019-08-22 10:13:09 +0200

    Updates

---
+++ precache-manifest.21f2120a47042194b4f214b547cf83e4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "017c75cdb680bd01afb7b779ab0b7413",
+    "url": "/golf/static/media/putterIcon.017c75cd.svg"
+  },
+  {
+    "revision": "aff82a3fa658881896559bc8a3894a4b",
+    "url": "/golf/static/media/ironIcon.aff82a3f.svg"
+  },
+  {
+    "revision": "c7fbd966482ddae2a68b96bc756e2555",
+    "url": "/golf/static/media/golfbag.c7fbd966.svg"
+  },
+  {
+    "revision": "deabfcc6139620e3ca6bc1d576a9f4ea",
+    "url": "/golf/static/media/fingerprint.deabfcc6.svg"
+  },
+  {
+    "revision": "664410f529fb7807c09affc6894d9d01",
+    "url": "/golf/static/media/driverIcon.664410f5.svg"
+  },
+  {
+    "revision": "c6d8013d8fdcf7e5019c",
+    "url": "/golf/static/js/runtime~main.c6d8013d.js"
+  },
+  {
+    "revision": "036856320f0741a2234e",
+    "url": "/golf/static/js/main.03685632.chunk.js"
+  },
+  {
+    "revision": "948f1476927f33c1c305",
+    "url": "/golf/static/js/1.948f1476.chunk.js"
+  },
+  {
+    "revision": "036856320f0741a2234e",
+    "url": "/golf/static/css/main.4ef61134.chunk.css"
+  },
+  {
+    "revision": "b3061cfbf81e0422320f093ad10c7825",
+    "url": "/golf/index.html"
+  }
+];
